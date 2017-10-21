0284594e3c1d7bcda048d68c9fc9643be62db1a1
Melody Wei <melodywei861016@gmail.com>
2017-10-21 12:07:20 -0700

    first commit

--- App.js
+++ App.js
@@ -1,7 +1,36 @@
 import React from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
-
 import { HomeStack } from './config/routes'
+
+class HomeScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Welcome',
+  };
+
+  render() {
+    const { navigate } = this.props.navigation;
+    return (
+      <View style={styles.container}>
+        <Text>DREAM</Text>
+        <Button
+          onPress={() => navigate('Courses')}
+          title="See Courses"
+          />
+        <Button
+          onPress={() => navigate('Students')}
+          title="See Students"
+          />
+      </View>
+    );
+  }
+}
+
+export const AppNavigator = StackNavigator({
+  Home    : { screen: HomeScreen },
+  Courses : { screen: CoursesScreen },
+  Students : { screen: StudentsScreen },
+});
+>>>>>>> first commit
 
 export default class App extends React.Component {
   render() {
