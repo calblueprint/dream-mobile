c00d9b03500cd54a120f69bec4c8db5195fec0c4
Melody Wei <melodywei861016@gmail.com>
2017-10-22 17:33:15 -0700

    mobile merge

--- config/routes.js
+++ config/routes.js
@@ -4,6 +4,7 @@ import { StackNavigator } from 'react-navigation';
 
 import HomeScreen from '../screens/HomeScreen';
 import CoursesScreen from '../screens/courses/CoursesScreen';
+import StudentsScreen from '../screens/students/StudentsScreen';
 
 
 export const HomeStack = StackNavigator({
@@ -19,6 +20,12 @@ export const HomeStack = StackNavigator({
       headerTitle: 'Courses',
     },
   },
+  Students : {
+    screen: StudentsScreen,
+    navigationOptions: {
+      headerTitle: 'Students',
+    },
+  },
 });
 
 
@@ -28,4 +35,5 @@ export class APIRoutes {
   static getCoursesPath()             { return APIRoutes.createRoute(`courses`) }
   // Example route w/ id
   // static createComponentPath(id)    { return APIRoutes.createRoute(`admins/subsections/${id}/components`) }
+  static getStudentsPath()             { return APIRoutes.createRoute(`students`) }
 }
