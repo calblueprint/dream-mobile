e64f1da3476871ee6256eb91520ab7ebea18c2a8
Casey Takahashi <caseytaka@berkeley.edu>
2017-10-23 16:40:03 -0700

    Create course screen.

--- screens/courses/CoursesScreen.js
+++ screens/courses/CoursesScreen.js
@@ -1,8 +1,12 @@
 import React from 'react';
+<<<<<<< 58b514217d98ba258abf14b35e8bec4a1ceefbc9
 import { Button, Text, View } from 'react-native';
 <<<<<<< db43e5c9b8ced305496d882b2f5267f5d491c181
 import { styles } from '../../styles/styles';
 =======
+=======
+import { Button, ScrollView, Text, View } from 'react-native';
+>>>>>>> Create course screen.
 import { styles } from '../../config/styles';
 <<<<<<< 7ad0dbc53b80825184c8056442d8a9d52e4e173f
 import { courseCardStyles } from '../../components/CourseCard';
@@ -61,13 +65,15 @@ class CoursesScreen extends React.Component {
       courses = this._renderCourses()
     }
     return (
-      <View>
-        <Button
-          onPress={() => navigate('CreateCourse')}
-          title="Create Course"
-        />
-        { courses }
-      </View>
+      <ScrollView>
+        <View>
+          <Button
+            onPress={() => navigate('CreateCourse', {refreshCourses: this._fetchCourses})}
+            title="Create Course"
+          />
+          { courses }
+        </View>
+      </ScrollView>
     );
 
   }
