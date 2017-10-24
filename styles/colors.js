332daf77f2f8e7c2736c4220f48772d5bead33fd
Vanessa Ng <vanesng96@gmail.com>
2017-10-23 17:32:13 -0700

    added general colors, text styles. created styles folder. added buttons component folder. added teacher screens folder.

---
+++ styles/colors.js
@@ -0,0 +1,28 @@
+import { StyleSheet } from 'react-native';
+
+const colors = StyleSheet.create({
+  primaryYellow: '#FFC107',
+  courseGreen: '#26A69A',
+  courseBlue: '#3F51B5',
+  coursePurple: '#7E57C2',
+  coursePink: '#C2185B',
+  courseBrown: '#795548',
+
+  successGreen: '#3BB273', 
+  lateOrange: '#FF8300', 
+  errorRed: '#E53935', 
+  errorPink: '#FFEAE9',
+
+  textDark: '#212121', 
+  textDark_70: 'rgba(33, 33, 33, .7)',
+  textDark_30: 'rgba(33, 33, 33, .3)',
+  textLight: '#FFF', 
+
+  backgroundGrey: '#F5F5F6', 
+  dividerGrey: '#E0E0E0', 
+  iconDark: 'rgba(33, 33, 33, .85)',
+  iconLight: 'rgba(255, 255, 255, .85)',
+
+});
+
+export { colors };
---
+++ styles/textStyles.js
@@ -0,0 +1,46 @@
+import { StyleSheet } from 'react-native';
+import colors from './colors';
+
+const textStyles = StyleSheet.create({
+  body: {
+    color: 'textDark',
+    fontSize: '20'
+  },
+
+  titleSmall: {
+    color: 'textDark_70',
+    fontSize: '16'
+  },
+
+  titleMedium: {
+    color: 'textDark',
+    fontSize; '20',
+    fontWeight: '700'
+  },
+  
+  titleLarge: {
+    color: 'textDark',
+    fontSize: '24', 
+    fontWeight: '700'
+  },
+  
+  buttonText: {
+    color: 'textLight',
+    fontSize: '20',
+    fontWeight: '500'
+  },
+
+  toastText: {
+    color: 'textLight',
+    fontSize: '20'
+  },
+
+  linkText: {
+    color: 'primaryYellow',
+    fontSize: '20'
+  },
+  
+
+});
+
+export { textStyles };
