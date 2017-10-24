5b7743da576d1322b85d919e0deea56d5dfc59fe
Kelsey Lam <kelseylam@berkeley.edu>
2017-10-23 20:00:19 -0700

    Update StudentsScreen to use new students route

--- screens/students/StudentsScreen.js
+++ screens/students/StudentsScreen.js
@@ -29,22 +29,6 @@ class StudentsScreen extends React.Component {
       console.error(error);
     }
     getRequest(APIRoutes.getStudentsPath(), successFunc, errorFunc);
-    fetch('http://127.0.0.1:3000/api/students', {
-      method: 'GET',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      }})
-      .then(function(response) {
-        return response.json();
-      })
-      .then(function(responseData) {
-        this.setState({ students: responseData, isLoading: false });
-      }.bind(this))
-      .catch(function(error) {
-        // TODO: Display correct toastr error msg
-        console.error(error);
-      });
   }
 
   _renderStudents() {
