7473d8234e7904a64277a6e0093a5a43c31dc609
Casey Takahashi <caseytaka@berkeley.edu>
2017-11-06 14:04:45 -0800

    Add toasters for updating a course

--- screens/courses/EditCourseScreen.js
+++ screens/courses/EditCourseScreen.js
@@ -6,6 +6,7 @@ import { APIRoutes } from '../../config/routes';
 import PropTypes from 'prop-types';
 import EditCourseForm from '../../components/Form/EditCourseForm'
 import { postRequest, putRequest } from '../../lib/requests';
+import DropdownAlert from 'react-native-dropdownalert';
 
 class EditCourseScreen extends React.Component {
   constructor(props) {
@@ -36,8 +37,10 @@ class EditCourseScreen extends React.Component {
       this.props.navigation.state.params.refreshCourses();
       this.props.navigation.goBack(null);
     }
-    const errorFunc = (error) => {
-      console.error(error);
+    const errorFunc = (response) => {
+      response.errors.map((error) => {
+        this.dropdown.alertWithType('error', 'Error', error);
+      });
     }
     course_id = this.props.navigation.state.params.course_id;
     putRequest(APIRoutes.getCoursePath(course_id), successFunc, errorFunc, params=params);
@@ -53,17 +56,20 @@ class EditCourseScreen extends React.Component {
       );
     } else {
       return (
-        <EditCourseForm
-          onSaveCourse={this._handleUpdateCourse.bind(this)}
-          is_active={navProps.is_active}
-          title={navProps.title}
-          teacher1={navProps.teacher1}
-          teacher2={navProps.teacher2}
-          weekday={navProps.weekday}
-          start_time={navProps.start_time}
-          end_time={navProps.end_time}
-          start_date={navProps.start_date}
-          end_date={navProps.end_date} />
+        <View>
+          <EditCourseForm
+            onSaveCourse={this._handleUpdateCourse.bind(this)}
+            is_active={navProps.is_active}
+            title={navProps.title}
+            teacher1={navProps.teacher1}
+            teacher2={navProps.teacher2}
+            weekday={navProps.weekday}
+            start_time={navProps.start_time}
+            end_time={navProps.end_time}
+            start_date={navProps.start_date}
+            end_date={navProps.end_date} />
+          <DropdownAlert ref={ref => this.dropdown = ref} />
+        </View>
       );
     }
   }
