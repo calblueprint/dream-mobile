6c79fabefcb86a87b256c25224a490f23bbe20ff
Casey Takahashi <caseytaka@berkeley.edu>
2017-11-18 12:22:20 -0800

    Add session button

--- components/Form/EditCourseForm.js
+++ components/Form/EditCourseForm.js
@@ -5,6 +5,8 @@ import { Form, InputField, PickerField,
 import { APIRoutes } from '../../config/routes';
 import PropTypes from 'prop-types';
 import { timeFormat } from '../../lib/time';
+import StyledButton from '../Button/Button';
+import Session from '../Session';
 
 /**
  * @prop onSaveCourse - callback function when course create form is submitted
@@ -12,16 +14,21 @@ import { timeFormat } from '../../lib/time';
 class EditCourseForm extends React.Component {
   constructor(props) {
     super(props);
+    this._handleFormChange = this._handleFormChange.bind(this);
+    this._mapSessions = this._mapSessions.bind(this);
+    this._renderAddSessionButton = this._renderAddSessionButton.bind(this);
     this.state = {
       is_active: this.props.is_active,
       title: this.props.title || '',
       teacher_id1: this.props.teacher1 || '',
       teacher_id2: this.props.teacher2 || '',
+      start_date: this.props.start_date || '',
+      end_date: this.props.end_date || '',
+      sessionList: this.props.sessionList || [],
+
       weekday: this.props.weekday || '',
       start_time: this.props.start_time || '',
       end_time: this.props.end_time || '',
-      start_date: this.props.start_date || '',
-      end_date: this.props.end_date || '',
     }
   }
 
@@ -40,21 +47,49 @@ class EditCourseForm extends React.Component {
   }
 
   /*
-   * Scroll to form field that is currently selected.
+   * Map session to its view in the Course form.
    */
-  _handleFormFocus(event, reactNode) {
-   this.refs.scroll.scrollToFocusedInput(event, reactNode);
+  _mapSessions(session, index) {
+    return (
+      <Session
+        weekday    = {session.weekday}
+        start_time = {session.start_time}
+        end_time   = {session.end_time}
+        key        = {session.id}
+        number     = {index}
+      />
+    );
+  }
+
+  _renderAddSessionButton() {
+    return (
+      <StyledButton
+        onPress={this._addSession}
+        text='Add Session'
+        clearButtonSmall>
+      </StyledButton>
+    );
   }
 
   render() {
     const today = new Date();
     const minDate = new Date(today.getFullYear()-2, today.getMonth(), today.getDate());
     const maxDate = new Date(today.getFullYear()+2, today.getMonth(), today.getDate());
+
+    // let sessions;
+    // if (this.state.isLoading) {
+    //   sessions = (
+    //     <Text>Loading...</Text>
+    //   )
+    // } else {
+    //   sessions = this.state.sessionList.map(this._mapSessions);
+    // }
+
     return (
       <View>
         <Form
           ref='registrationForm'
-          onChange={this._handleFormChange.bind(this)}
+          onChange={this._handleFormChange}
           label="Personal Information">
           <InputField
             ref='title'
@@ -73,6 +108,22 @@ class EditCourseForm extends React.Component {
             label='Teacher ID 2'
             value={this.state.teacher_id2}
             placeholder='e.g. 12634669'/>
+
+          <DatePickerField
+            ref='start_date'
+            minimumDate={minDate}
+            maximumDate={maxDate}
+            mode="date"
+            date={this.state.start_date}
+            placeholder='Start Date'/>
+
+          <DatePickerField
+            ref='end_date'
+            minimumDate={minDate}
+            maximumDate={maxDate}
+            mode='date'
+            date={this.state.end_date}
+            placeholder='End Date'/>
 
           <PickerField
             ref='weekday'
@@ -100,21 +151,7 @@ class EditCourseForm extends React.Component {
             date={this.state.end_time}
             placeholder='Session End'/>
 
-          <DatePickerField
-            ref='start_date'
-            minimumDate={minDate}
-            maximumDate={maxDate}
-            mode="date"
-            date={this.state.start_date}
-            placeholder='Start Date'/>
-
-          <DatePickerField
-            ref='end_date'
-            minimumDate={minDate}
-            maximumDate={maxDate}
-            mode='date'
-            date={this.state.end_date}
-            placeholder='End Date'/>
+          { this._renderAddSessionButton() }
         </Form>
         <Button
           onPress={() => this.props.onSaveCourse({ course: this.state })}
