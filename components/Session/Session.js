338dd4e13897260e2f985476a03e16ae5d76bcbb
Casey Takahashi <caseytaka@berkeley.edu>
2017-11-18 12:22:20 -0800

    Add session button

---
+++ components/Session/Session.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Image, View, Text, StyleSheet, Button } from 'react-native';
+import styles from './styles'
+import { textStyles } from '../../styles/textStyles';
+
+class Session extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    }
+  }
+
+  render() {
+    return (
+      <View>
+        <Form
+          ref='sessionForm'
+          onChange={this._handleFormChange.bind(this)}>
+
+          <PickerField
+            ref='weekday'
+            options={{
+              'Sunday': 'Sunday',
+              'Monday': 'Monday',
+              'Tuesday': 'Tuesday',
+              'Wednesday': 'Wednesday',
+              'Thursday': 'Thursday',
+              'Friday': 'Friday',
+              'Saturday': 'Saturday',
+            }}
+            value={this.props.weekday}
+            label='Select Day'/>
+
+          <TimePickerField
+            ref='start_time'
+            dateTimeFormat={timeFormat}
+            date={this.props.start_time}
+            placeholder='Session Start'/>
+
+          <TimePickerField
+            ref='end_time'
+            dateTimeFormat={timeFormat}
+            date={this.props.end_time}
+            placeholder='Session End'/>
+        </Form>
+      </View>
+    );
+  }
+}
+
+export default Session;
