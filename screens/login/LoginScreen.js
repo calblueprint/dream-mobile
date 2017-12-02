82ffddca0387a9f342e9373d186cb97ffe8a865e
Casey Takahashi <caseytaka@berkeley.edu>
2017-12-02 00:15:18 -0800

    Revert login screen

--- screens/login/LoginScreen.js
+++ screens/login/LoginScreen.js
@@ -8,7 +8,7 @@ import { styles } from '../../styles/styles';
 import { postRequest } from '../../lib/requests';
 import { APIRoutes } from '../../config/routes';
 import { standardError } from '../../lib/alerts';
-import LoginForm from '../../components/Form/LoginForm'
+
 
 class LoginScreen extends React.Component {
   constructor(props) {
@@ -31,22 +31,25 @@ class LoginScreen extends React.Component {
 
   render() {
     return (
-      <TextInput
-        placeholder='Email'
-        autoCapitalize='none'
-        onChangeText={(text) => this.setState({email: text})}/>
-      <TextInput
-        placeholder='Password'
-        onChangeText={(text) => this.setState({password: text})}
-        secureTextEntry/>
-      <Button
-        onPress={this._attemptLogin.bind(this)}
-        title='Login'
-      />
-      <Button
-        onPress={() => this.props.navigation.navigate('SignUp')}
-        title='Sign Up'
-      />
+      <View>
+        <TextInput
+          placeholder='Email'
+          autoCapitalize='none'
+          onChangeText={(text) => this.setState({email: text})}/>
+        <TextInput
+          placeholder='Password'
+          onChangeText={(text) => this.setState({password: text})}
+          secureTextEntry/>
+        <Button
+          onPress={this._attemptLogin.bind(this)}
+          title='Login'
+        />
+        <Button
+          onPress={() => this.props.navigation.navigate('SignUp')}
+          title='Sign Up'
+        />
+
+      </View>
     );
   }
 }
