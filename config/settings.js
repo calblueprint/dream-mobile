6df359857028d107af6dc9d75e9aa81904475816
Vanessa Ng <vanesng96@gmail.com>
2018-02-14 20:28:46 -0800

    config mobile phone url

--- config/settings.js
+++ config/settings.js
@@ -5,13 +5,16 @@ import { Platform } from 'react-native'
  */
  // TODO (Kelsey): test whether if check actually works
 if (process.env.NODE_ENV === 'production') {
+	console.log('here1')
   URL = 'https://dream-rails-production.herokuapp.com';
 } else {
-  URL = Platform.select({
-    ios: "http://localhost:3000",
-    // For Android Emulator
-    android: "http://10.0.2.2:3000"
-  });
+	console.log('here2')
+  URL = "http://10.142.150.79:3000"
+  // URL = Platform.select({
+  //   ios: "http://localhost:3000",
+  //   // For Android Emulator
+  //   android: "http://10.0.2.2:3000"
+  // });
 }
 
 export const settings = {
