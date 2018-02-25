23743346c006713157021979d9996fd9ad836789
Justin Mi <justin.mi@berkeley.edu>
2018-02-24 17:17:08 -0800

    modified initial store to accomodate redux-offline

--- config/store.js
+++ config/store.js
@@ -1,22 +1,18 @@
-import storage from 'redux-persist/lib/storage'
-import { persistStore, persistCombineReducers } from 'redux-persist';
-import { createStore, applyMiddleware } from 'redux';
+import { applyMiddleware, createStore, combineReducers } from 'redux';
+import { offline } from '@redux-offline/redux-offline';
+import defaultConfig from '@redux-offline/redux-offline/lib/defaults';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import reducers from '../reducers';
 import thunk from 'redux-thunk';
 
-const config = {
-  key: 'root',
-  blacklist: ['isLoading', 'modal'],
-  storage,
-}
+const reducer = combineReducers(reducers);
+const initialState = { isLoading: {value: true} };
 
-const reducer = persistCombineReducers(config, reducers)
-const initialState = { isLoading: {value: true} }
-
-export const configureStore = () => {
-  const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(thunk)));
-  const persistor = persistStore(store);
-
-  return { persistor, store };
-}
+export const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(
+    applyMiddleware(thunk),
+    offline(defaultConfig)
+  )
+);
