99cb810a721be4ed30f6d38d1670473d829f5524
Justin Mi <justin.mi@berkeley.edu>
2018-03-07 20:35:52 -0800

    added Toaster code

---
+++ components/Toaster/index.js
@@ -0,0 +1,9 @@
+/**
+ * Credit: https://github.com/tableflip/react-native-toaster
+ */
+
+import Toaster from './Toaster'
+import ToastStyles from './ToastStyles'
+
+export default Toaster
+export { ToastStyles }
