b1652bab97c37ef1d351b4f4f71b52939ff979da
Vanessa Ng <vanesng96@gmail.com>
2018-03-11 23:48:09 -0700

    small typo in settings.js

--- config/settings.js
+++ config/settings.js
@@ -5,7 +5,7 @@ import { Platform } from 'react-native'
  */
  // TODO (Kelsey): test whether if check actually works
 if (process.env.NODE_ENV === 'production') {
-  // URL = 'https://dream-rails-production.herokuapp.com';
+  URL = 'https://dream-rails-production.herokuapp.com';
 } else {
   URL = Platform.select({
     ios: "http://localhost:3000",
