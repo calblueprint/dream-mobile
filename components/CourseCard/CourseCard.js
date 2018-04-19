24a526e3cacd8f361b145316983f61c83a968c2e
Vanessa Ng <vanesng96@gmail.com>
2018-04-19 14:54:22 -0700

    added passing color into viewcoursescreen commit

--- components/CourseCard/CourseCard.js
+++ components/CourseCard/CourseCard.js
@@ -31,7 +31,7 @@ class CourseCard extends React.Component {
     const colorKey = this.props.index % 5
     let syncText = this.props.synced ? "" : "Not Synced"
     return (
-      <TouchableHighlight onPress={() => this.props.onSelectCourse(this.props.course_id)}>
+      <TouchableHighlight onPress={() => this.props.onSelectCourse(this.props.course_id, colorKey)}>
         <View style={[cardStyles.outerContainer, {backgroundColor: colorList[colorKey]}]}>
           <View style={cardStyles.topContainer}>
             <Text style={[cardStyles.title, textStyles.titleMediumLight]}>{this.props.title}</Text>
